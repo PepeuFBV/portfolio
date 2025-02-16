ba66b76d9056c485501369470b92708dd5a6ffe3
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-02-16 16:53:43 -0300

    Content placement fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Home from '@/components/Home'
 
 export default function Main() {
     return (
-        <main className="grid grid-cols-2 h-screen w-full items-center justify-between my-10 py-20 px-56">
+        <main className="grid grid-cols-2 h-screen w-full items-center justify-between pt-28 pb-12 px-56">
             <Home />
             <p className='w-full h-full'>right content</p>
         </main>
