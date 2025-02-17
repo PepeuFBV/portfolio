8dd32e11443b2e9e5eb2e3d7d32797281d8a86de
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-02-17 08:22:56 -0300

    Responsiveness fixes

--- src/components/core/Home/index.tsx
+++ src/components/core/Home/index.tsx
@@ -15,12 +15,13 @@ const Home: React.FC<HomeProps> = ({ className, languagePos = 0 }) => {
     return (
         <section className={cn('flex flex-col w-full h-full justify-between gap-8 lg:gap-0', className)}>
             <div className='flex flex-col gap-16'>
-                <div className='w-full flex flex-col gap-5 lg:gap-10'>
+                <div className='lg:w-full flex flex-col gap-5 lg:gap-10'>
                     <div className='flex gap-6 items-center justify-start'>
                         <motion.div
                             initial={{ opacity: 0, x: -30 }}
                             animate={{ opacity: 1, x: 0 }}
                             transition={{ duration: 0.5, delay: 0 }}
+                            className='hidden md:block'
                         >
                             <Image src="/pedro.jpg" alt="Logo" width={150} height={150} loading='eager' quality={100} className='rounded-full float-left' />
                         </motion.div>
@@ -49,12 +50,12 @@ const Home: React.FC<HomeProps> = ({ className, languagePos = 0 }) => {
                         initial={{ opacity: 0, y: 50 }}
                         animate={{ opacity: 1, y: 0 }}
                         transition={{ duration: 0.5, delay: 1.3 }}
-                        className='text-start font-extralight text-md w-[66%]'
+                        className='text-start font-extralight text-md w-[58%]'
                     >
                         {aboutme.smallDescription[languagePos]}
                     </motion.p>
                 </div>
-                <div className='md:visible hidden'>
+                <div className='hidden lg:block'>
                     <p>todo: index</p>
                 </div>
             </div>
